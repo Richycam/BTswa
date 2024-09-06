8281b2e56190c23182dcdfb9e2d5c22214a47b07
Richy <97134605+Richycam@users.noreply.github.com>
2024-09-06 23:21:00 +0100

    Update BTswa.py

--- BTswa.py
+++ BTswa.py
@@ -1,3 +1,5 @@
+#//     TODO Add more stuff 
+from siem import siem
 import time
 import os
 import sys
@@ -73,19 +75,12 @@ L___________________J
    
 """
 
-class siem:
-    def __init__(self, tool, link):
-        self.tool = tool
-        self.link = link
 
-    def call_back(tool):
-        print(tool)
 
-    def call_back(link):
-        print(link)
 
 def clear():
     os.system("clear")
+    os.system("cls")
 
 def for_art():
     list = [art1, art2, art3, art4]
@@ -97,53 +92,91 @@ def for_art():
 def exit():
     sys.exit()
 
-clear()
-for_art()
-tool = input("tool name? \n")
-link = input("tool link? \n")
-siem(tool,link)
-clear()
-print(Banner)
+
 
 
 def menu():
 
     print("--------------------------------------------")
-    siem.call_back(tool)
+    print(siem.get_tool())    
     print("1) siem link ")
     print("2) siem documents")
+    print("3) Nmap")
+    print("4) ")
     print("--------------------------------------------")
 
-menu()
 
 def main():
-    main_cntrl = input("choose useage \n")
-    if main_cntrl == "1":
-        clear()
-        siem.call_back(link)
-        back = input("go back? Y/N?")
-        if back == "y" or "Y":
+    menu()
+    main_cntrl = input("choose usage \n")
+    match main_cntrl:
+        case "1":
             clear()
-            print(Banner)
-            menu()
-            main()
-
-
-    elif main_cntrl == "2":
-        if siem.call_back(tool) == "elastic"  or "kibana" or "Elastic" or "Kibana":
-            print("https://www.elastic.co/guide/index.html?utm_campaign=Google-B-EMEA-UKI-Exact&utm_content=Brand-Core-Documentation&utm_source=google&utm_medium=cpc&device=c&utm_term=elastic%20documentation&gad_source=1&gclid=EAIaIQobChMIsuHa15OuiAMV6JtQBh2tHgypEAAYASAAEgKobvD_BwE") 
-            back = input("go back? Y/N?")
-            if back == "y" or "Y":
+            print(siem.get_tool())
+            back = input("go back? Y/N? \n ").lower
+            if back == "y":
                 clear()
                 print(Banner)
-                menu()
                 main()
-        elif siem.call_back(tool) == "Splunk" or "splunk":
-            print("https://docs.splunk.com/Documentation") 
-            back = input("go back? Y/N?")
-            if back == "y" or "Y":
-                clear()
-                print(Banner)
-                menu()
-                main()
+
+        case "2":
+            docs = siem.get_tool()
+            if docs == "kibana" or docs == "elastic": 
+                    print("https://www.elastic.co/guide/index.html?utm_campaign=Google-B-EMEA-UKI-Exact&utm_content=Brand-Core-Documentation&utm_source=google&utm_medium=cpc&device=c&utm_term=elastic%20documentation&gad_source=1&gclid=EAIaIQobChMIsuHa15OuiAMV6JtQBh2tHgypEAAYASAAEgKobvD_BwE") 
+                    back = input("go back? Y/N?")
+                    if back == "y":
+                        clear()
+                        print(Banner)
+                        main()
+            elif docs == "splunk":
+                    print("https://docs.splunk.com/Documentation") 
+                    back = input("go back? Y/N?").lower()
+                    if back == "y":
+                        clear()
+                        print(Banner)
+                        main()
+            elif docs == "velociraptor":
+                print("https://docs.velociraptor.app/docs/")
+
+
+        case "3":
+            clear()
+            ip = input("ip to scan \n")
+            print("scan type") 
+            print("1) simple scan")
+            print("2) version detector")
+            scan_type = input("")
+            match scan_type:
+                case "1":
+                    clear()
+                    simple_scan = "nmap {0}".format(ip)
+                    os.system(simple_scan)
+                    back = input("want to go back? Y/N? \n").lower()
+                    if back == "Y":
+                        clear()
+                        print(Banner)
+                        main()
+                case "2":
+                    clear()
+                    ver_scan = "nmap {0} -A".format(ip)
+                    os.system(ver_scan)
+                    back = input("want to go back Y/N? \n")
+                    if back == "Y":
+                        clear()
+                        print(Banner)
+                        main()                            
+
+        case "4":
+            #//     TODO Add to this 
+            clear()
+
+
+clear()
+for_art()
+tool = input(str("tool name? \n")).lower()
+link = input("tool link? \n").lower()
+siem = siem(tool,link)
+clear()
+print(Banner)
 main()
+
