36d77cd41d72d74777b030cc084081e2196de538
Richy <97134605+Richycam@users.noreply.github.com>
2024-09-06 23:21:42 +0100

    Create siem.py

---
+++ siem.py
@@ -0,0 +1,10 @@
+class siem:
+    def __init__(self , tool, link):
+        self.tool = tool
+        self.link = link
+
+    def get_tool(self):
+        return self.tool
+    def get_link(self):
+        return self.link
+        
