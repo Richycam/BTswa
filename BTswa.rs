9e3f2a251e3bc94f95937b713259e49ac1d0542f
Richy <97134605+Richycam@users.noreply.github.com>
2024-09-07 22:37:42 +0100

    Update BTswa.rs

--- BTswa.rs
+++ BTswa.rs
@@ -1,25 +1,37 @@
 //using this as a way to learn rust please dont PR on this 
 
-fn read_string() -> String {
-   let mut input = String::new();
+fn read_string_tool() -> String {
+   let mut tool = String::new();
    std::io::stdin()
        .read_line(&mut input)
        .expect("can not read user input");
-   input
+   tool
 }
+fn read_string_link(){
+   let mut link = String::new();
+   std::io::stdin()
+       .read_line(&mut input)
+       .expect("can not read user input");
+   link
+}
+
+
+
 fn inital(){
    println!("what is your siem?");
-   let input = read_string();
-   }
+   let tool = read_string_tool();
+   println!("What is the siem link?")
+   let link = read_string_link(); 
+
+}
 
 fn menu(){
    println!("--------------------------------------------");   
    println!("1) siem link ");
    println!("2) siem documents");
    println!("3) Nmap");
-   println!("4) ");
    println!("--------------------------------------------");
-   }
+}
 
 fn main(){
   
