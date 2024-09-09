c7c6db1eb179f80437721e9abb8db286a1121125
Richy <97134605+Richycam@users.noreply.github.com>
2024-09-09 21:55:27 +0100

    Create btswa.cpp

---
+++ c++/btswa.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+    cout << "--------------------------------------------" <<endl;   
+    cout << "1) siem link "<<endl;
+    cout << "2) siem documents"<<endl;
+    cout << "3) Nmap"<<endl;
+    cout << "--------------------------------------------"<<endl;
+
+
+
+int choose; 
+cout << "choose :  "; 
+cin >> choose; 
+int stoi(choose);
+if (choose == 1){
+    cout << "test" <<endl;
+return 0;
+} 
+}
