bffad98dec7cd73e67d39817c8d213ef9a5b8197
Richy <97134605+Richycam@users.noreply.github.com>
2024-09-13 11:46:45 +0100

    Update btswa.cpp

--- c++/btswa.cpp
+++ c++/btswa.cpp
@@ -136,11 +136,19 @@ the_menu_obj.start3 = "2) siem documents";
 the_menu_obj.start4 = "3) Nmap";
 the_menu_obj.start5 = "--------------------------------------------";
 // i lost my sanity ok
+
+
+
+
+bool looping = true;
+while(looping == true){
 cout << the_menu_obj.start1 <<endl; 
 cout << the_menu_obj.start2 <<endl; 
 cout << the_menu_obj.start3 <<endl; 
 cout << the_menu_obj.start4 <<endl; 
 cout << the_menu_obj.start5 <<endl; 
+
+
 
 
 
@@ -151,16 +159,27 @@ int choose;
 if (choose == 1){ 
     system("clear");
         cout << siem_obj.link <<endl;
-    
+        cout << "\n";
+        sleep(10);
+        system("clear");
+        continue;
 
 } else if (choose == 2){   
         system("clear");
         if (siem_obj.tool == "kibana"){
-                cout << "https://www.elastic.co/guide/en/kibana/current/index.html";        
+                cout << "https://www.elastic.co/guide/en/kibana/current/index.html"<<endl;
+                sleep(10);
+                system("clear");
+                continue;
+                
+
+
         } else if (siem_obj.tool == "splunk"){
-            cout << "https://docs.splunk.com/Documentation";
-            }
-} else if (choose == 3){
+            cout << "https://docs.splunk.com/Documentation" <<endl;
+            sleep(10);
+            system("clear");
+            continue;
+} else if (siem_obj.tool == 3){
         int nmap;
             cout << "scan type?" <<endl;
             cout << "1) simple " <<endl;
@@ -169,8 +188,16 @@ if (choose == 1){
         int ip; 
             cout << "ip address to scan?"<<endl;
                 cin >> ip;
-        }
 
+        sleep(10);
+        system("clear");
+        continue;
+            
+        }
+}//while loop 
 return 0;
-    }           
+    }
+}           
+
+
 
