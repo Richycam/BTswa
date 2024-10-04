1608b0273b2df49222b1f66b476246a85962eaef
Richy <97134605+Richycam@users.noreply.github.com>
2024-10-04 19:34:40 +0100

    Update siem.py

--- python/siem.py
+++ python/siem.py
@@ -1,10 +1,1 @@
-class siem:
-    def __init__(self , tool, link):
-        self.tool = tool
-        self.link = link
 
-    def get_tool(self):
-        return self.tool
-    def get_link(self):
-        return self.link
-        
